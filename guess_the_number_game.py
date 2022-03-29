2daaaa5b99cc0157e418ededc4412add97ffc221
bhuvaneswar370 <bhuvaneswr370@gmail.com>
2022-03-29 12:51:19 +0530

    comments add to guess the number game

--- guess_the_number_game.py
+++ guess_the_number_game.py
@@ -5,6 +5,7 @@ num = random.randint(1, 10)
 guess = int(input('Guess a number between 1 and 10'))
 # Add while loop here
 while guess != num:
+# incremently assign the input to guess variable; it iterates until the "guess != num"
     guess = int(input('Guess again'))
 
 print('You win!') 
